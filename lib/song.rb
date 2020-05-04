a4054eb38f7fcaf09469c4f175a4e3f15df74f02
Roland McRae <mcraeroland@gmail.com>
2020-05-04 06:56:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@artists = 0
-  @@genres = 0
+  @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << @genre
   end
 
 
